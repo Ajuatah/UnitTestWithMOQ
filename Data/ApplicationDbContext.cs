a37492b841fd03e5447f11f172a37078098fa6fe
andropov.ajebua <andropov.ajebua@cm.technofex.com>
2023-05-09 11:05:55 +0100

    Add project files.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UnitTestWithMOQ.API.Models;
+
+namespace UnitTestWithMOQ.API.Data
+{
+    
+    public class ApplicationDbContex : DbContext
+    {
+        protected readonly IConfiguration Configuration;
+
+        public ApplicationDbContex(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));
+        }
+        public DbSet<Product> Products { get; set; }
+    }
+}
